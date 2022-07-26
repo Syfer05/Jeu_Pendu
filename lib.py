3afc30154205ad328b29463705e1144dcf1eb3c1
Syfer05 <syfer@gmx.fr>
2022-07-26 21:19:43 +0200

    Add:Clean code & syntax, display() à revoir

--- lib.py
+++ lib.py
@@ -1,6 +1,6 @@
 import random
-essais = 10
-lifes = essais
+essais = 10  # Variable constante de référence, désigne le nombre de tentative possible
+lifes = essais  # Variable évolutive désigne le nombre de tentative restante
 
 def get_word(niveau):
     # Le dictionnaire(apparemment un dico doit avoir une unicité dans ses clés, du coup j'ai inversé key & value)
@@ -12,42 +12,44 @@ def get_word(niveau):
         "terminaison": 1,
         "allumette": 1
     }
+
     # Création d'un nouveau dictionnaire contenant uniquement des clés et valeurs,
     # pour lesquelles la valeur est égale au niveau choisi par le joueur
     dico_b = {}
     for key, value in dico_a.items():
         if value == int(niveau):
             dico_b[key] = value
-    # choix d'un mot du dico grace à la méthode random
+
+    # choix d'un mot du dico_b grace à la méthode random
     mot = random.choice(list(dico_b.keys()))
     return mot
 
 
 def display(lettre,terme,evolution):
-    longueur = len(terme)
+    #longueur = len(terme)
     liste = list(terme)
     traits = []
-
+# Need correction
     if evolution == "":
-        for x in range(longueur):
+        for x in range(len(terme)):
             traits.append("_")
         #print(traits)
     else:
         traits = list(evolution)
         lettre = input("Entrez votre lettre:\n")
-
+# Need correction
     ultimo = match_word(liste,lettre,traits,terme)
-    if ultimo != "win" and ultimo != "end":
-        print(ultimo) # Affichage de mon évolution de lettres dans le jeu
+    if ultimo != "win" and ultimo != "end": # Tant que le jeu n'est pas fini il y a,
+        print(ultimo)  # affichage de mon évolution de lettres dans le jeu
     return ultimo
 
 
 def match_word(suite, caractere, evo, chaine):
     coeurs = "Nombre de tentatives restantes: "
-    bad_letter="-> Lettre proposée non incluse dans le mot!"
+    bad_letter = "-> Lettre proposée non incluse dans le mot!"
 
     global lifes
-    #fonction de vérification des conditions suivantes:
+    # fonction de vérification des conditions suivantes:
     # nombre de tentatives restantes
     # la lettre proposée est-elle dans le mot?
 
@@ -62,10 +64,11 @@ def match_word(suite, caractere, evo, chaine):
         lifes -= 1
     print(coeurs, lifes)
     #print(evo, list(chaine))
+
     # renvoi d'une valeur "win", "lose" ou "les lettres actuellement découvertes"
     if evo == list(chaine):
         end = "win"
-        print(''.join(evo))
+        print(''.join(evo))  # affichage de liste sans ' , [ ] et espace
         return end
     elif lifes == 0:
         end = "lose"
@@ -80,12 +83,12 @@ def print_menu():
     difficulty = "Veuillez choisir votre niveau de difficulté!\n 1:Easy \n 2:Normal \n 3:Hard \n q:Quit \n"
     winner = "Bravo vous avez remporté la victoire!"
     loser = "Désolé de cet échec"
-    again = "Souhaitez-vous refaire une partie? (O)ui ou (N)on \n"
+    again = "\n Souhaitez-vous refaire une partie? (O)ui ou (N)on \n"
     deviner = "Veuillez entrer une première lettre\n"
     bye = "Au revoir!"
 
     print(welcome_message)
-    level = input(difficulty) # Demander au joueur de choisir le level de difficulté
+    level = input(difficulty)  # Demander au joueur de choisir le level de difficulté
 
 
     ### Appel de la Function -> get_word()
@@ -115,7 +118,7 @@ def print_menu():
         exit()
 
 
-    stop_or_continue = input(again) # Demander au joueur s'il veux rejouer ou quitter
+    stop_or_continue = input(again)  # Demander au joueur s'il veux rejouer ou quitter
 
     if stop_or_continue in 'oO':
         global lifes
@@ -124,4 +127,4 @@ def print_menu():
         print_menu()
     elif stop_or_continue in 'nN':
         print(bye)
-        exit()# Sortie du programme
+        exit()  # Sortie du programme
