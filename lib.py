7f344192618bcb8ecf79f2cd15f88f92c320c0d9
Syfer05 <syfer@gmx.fr>
2022-07-26 21:09:48 +0200

    Add:Affichage traits avant input lettre & clean code

--- lib.py
+++ lib.py
@@ -5,12 +5,12 @@ lifes = essais
 def get_word(niveau):
     # Le dictionnaire(apparemment un dico doit avoir une unicité dans ses clés, du coup j'ai inversé key & value)
     dico_a = {
-        "lait": 1,
-        "pour": 1,
+        "lait": 3,
+        "pour": 3,
         "flamme": 2,
         "positif": 2,
-        "terminaison": 3,
-        "allumette": 3
+        "terminaison": 1,
+        "allumette": 1
     }
     # Création d'un nouveau dictionnaire contenant uniquement des clés et valeurs,
     # pour lesquelles la valeur est égale au niveau choisi par le joueur
@@ -31,37 +31,41 @@ def display(lettre,terme,evolution):
     if evolution == "":
         for x in range(longueur):
             traits.append("_")
-        print(traits)
+        #print(traits)
     else:
         traits = list(evolution)
         lettre = input("Entrez votre lettre:\n")
 
     ultimo = match_word(liste,lettre,traits,terme)
-    print(ultimo)
+    if ultimo != "win" and ultimo != "end":
+        print(ultimo) # Affichage de mon évolution de lettres dans le jeu
     return ultimo
 
 
 def match_word(suite, caractere, evo, chaine):
+    coeurs = "Nombre de tentatives restantes: "
+    bad_letter="-> Lettre proposée non incluse dans le mot!"
+
     global lifes
     #fonction de vérification des conditions suivantes:
     # nombre de tentatives restantes
     # la lettre proposée est-elle dans le mot?
-    bad_letter="Lettre proposée non incluse dans le mot!"
+
     v = 0
     if caractere in suite:
         for x in suite:
             if caractere == x:
-                print(v)
                 evo[v] = caractere
             v += 1
     else:
         print(bad_letter)
         lifes -= 1
-    print(lifes)
-    print(evo, list(chaine))
+    print(coeurs, lifes)
+    #print(evo, list(chaine))
     # renvoi d'une valeur "win", "lose" ou "les lettres actuellement découvertes"
     if evo == list(chaine):
         end = "win"
+        print(''.join(evo))
         return end
     elif lifes == 0:
         end = "lose"
@@ -74,8 +78,8 @@ def match_word(suite, caractere, evo, chaine):
 def print_menu():
     welcome_message = "---Bienvenue sur le Jeu du pendu!---"
     difficulty = "Veuillez choisir votre niveau de difficulté!\n 1:Easy \n 2:Normal \n 3:Hard \n q:Quit \n"
-    winner = "Bravo vous avez remporté la victoire et découvert le mot x en x tentatives"
-    loser = "Désolé de cet échec, le mot à découvrir était : x"
+    winner = "Bravo vous avez remporté la victoire!"
+    loser = "Désolé de cet échec"
     again = "Souhaitez-vous refaire une partie? (O)ui ou (N)on \n"
     deviner = "Veuillez entrer une première lettre\n"
     bye = "Au revoir!"
@@ -86,14 +90,19 @@ def print_menu():
 
     ### Appel de la Function -> get_word()
     if level not in 'qQ':
+        debut = []
         fin = ""
         ### Appel de la fonction get_word avec paramétre level
         word = get_word(level)
+
+        for x in range(len(word)):
+            debut.append("_")
+        print(''.join(debut))
         #print("Le mot c'est:", word)
         ### Appel de la Function -> display()
         letter = input(deviner)
         fin = display(letter, word, fin)
-        print(fin)
+        #print(fin)
 
         while not (fin == "win") and not (fin == "lose"):
             fin = display(letter, word, fin)
